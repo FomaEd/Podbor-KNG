f4c576adf8ea25381347020b8959a01ea1482491
FomaEd <164784372+FomaEd@users.noreply.github.com>
2026-04-06 11:34:30 +0300

    Update lift-slide.js

--- data/lift-slide.js
+++ data/lift-slide.js
@@ -1,1 +1,125 @@
-//Пустой
+// Ручки для раздвижных систем Lift-Slide
+ const liftSlideHandleCatalog = {
+  one_side_concise: {
+    white: [
+      { article: 'KN218MN_9016', name: 'Ручка Concise для раздвиж. двери под квадратный штифт белая', qty: 1 },
+      { article: 'KN208AA_9016', name: 'Ручка Concise врезная внешняя (Ракушка) белая', qty: 1 },
+      { article: 'KN100SO-100', name: 'Штифт 100 мм', qty: 1 },
+      { article: 'KN100TN-90', name: 'Пара винтов', qty: 1 },
+    ],
+    black: [
+      { article: 'KN218MN_9005M', name: 'Ручка Concise для раздвиж. двери под квадратный штифт чёрная', qty: 1 },
+      { article: 'KN208AA_9005M', name: 'Ручка Concise врезная внешняя (Ракушка) чёрная', qty: 1 },
+      { article: 'KN100SO-100', name: 'Штифт 100 мм', qty: 1 },
+      { article: 'KN100TN-90', name: 'Пара винтов', qty: 1 },
+    ],
+    raw: [
+      { article: 'KN218MN_C', name: 'Ручка Concise для раздвиж. двери под квадратный штифт неокрашенная', qty: 1 },
+      { article: 'KN208AA_C', name: 'Ручка Concise врезная внешняя (Ракушка) неокрашенная', qty: 1 },
+      { article: 'KN100SO-100', name: 'Штифт 100 мм', qty: 1 },
+      { article: 'KN100TN-90', name: 'Пара винтов', qty: 1 },
+    ],
+  },
+  two_side_concise: {
+    white: [
+      { article: 'KN218MO_9016', name: 'Ручка Concise для раздвиж. двери под квадратный штифт белая', qty: 1 },
+      { article: 'KN218NG_9016', name: 'Ручка врезная внешняя под квадратный штифт белая', qty: 1 },
+      { article: 'KN100SO-115', name: 'Штифт 115 мм', qty: 1 },
+      { article: 'KN100TN-90', name: 'Пара винтов', qty: 1 },
+    ],
+    black: [
+      { article: 'KN218MO_9005M', name: 'Ручка Concise для раздвиж. двери под квадратный штифт чёрная', qty: 1 },
+      { article: 'KN218NG_9005M', name: 'Ручка врезная внешняя под квадратный штифт чёрная', qty: 1 },
+      { article: 'KN100SO-115', name: 'Штифт 115 мм', qty: 1 },
+      { article: 'KN100TN-90', name: 'Пара винтов', qty: 1 },
+    ],
+    raw: [
+      { article: 'KN218MO_C', name: 'Ручка Concise для раздвиж. двери под квадратный штифт неокрашенная', qty: 1 },
+      { article: 'KN218NG_C', name: 'Ручка врезная внешняя под квадратный штифт неокрашенная', qty: 1 },
+      { article: 'KN100SO-115', name: 'Штифт 115 мм', qty: 1 },
+      { article: 'KN100TN-90', name: 'Пара винтов', qty: 1 },
+    ],
+  },
+  one_side_master: {
+    white: [
+      { article: 'KN218NK_9016', name: 'Ручка Master Pro LS внутренняя белая', qty: 1 },
+      { article: 'KN208AA_9016', name: 'Ручка Concise врезная внешняя (ракушка) белая', qty: 1 },
+      { article: 'KN100SO-100', name: 'Штифт 100 мм', qty: 1 },
+      { article: 'KN100TN-90', name: 'Пара винтов', qty: 1 },
+    ],
+    black: [
+      { article: 'KN218NK_9005M', name: 'Ручка Master Pro LS внутренняя чёрная', qty: 1 },
+      { article: 'KN208AA_9005M', name: 'Ручка Concise врезная внешняя (ракушка) чёрная', qty: 1 },
+      { article: 'KN100SO-100', name: 'Штифт 100 мм', qty: 1 },
+      { article: 'KN100TN-90', name: 'Пара винтов', qty: 1 },
+    ],
+    raw: [
+      { article: 'KN218NK_C', name: 'Ручка Master Pro LS внутренняя неокрашенная', qty: 1 },
+      { article: 'KN208AA_C', name: 'Ручка Concise врезная внешняя (ракушка) неокрашенная', qty: 1 },
+      { article: 'KN100SO-100', name: 'Штифт 100 мм', qty: 1 },
+      { article: 'KN100TN-90', name: 'Пара винтов', qty: 1 },
+    ],
+  },
+  two_side_master: {
+    white: [
+      { article: 'KN218NT_9016', name: 'Ручка Master Pro LS внутренняя под цилиндр белая', qty: 1 },
+      { article: 'KN218NU_9016', name: 'Ручка Master Pro LS внешняя под цилиндр белая', qty: 1 },
+      { article: 'KN100SO-115', name: 'Штифт 115 мм', qty: 1 },
+      { article: 'KN100TN-90', name: 'Пара винтов', qty: 1 },
+    ],
+    black: [
+      { article: 'KN218NT_9005M', name: 'Ручка Master Pro LS внутренняя под цилиндр чёрная', qty: 1 },
+      { article: 'KN218NU_9005M', name: 'Ручка Master Pro LS внешняя под цилиндр чёрная', qty: 1 },
+      { article: 'KN100SO-115', name: 'Штифт 115 мм', qty: 1 },
+      { article: 'KN100TN-90', name: 'Пара винтов', qty: 1 },
+    ],
+    raw: [
+      { article: 'KN218NT_C', name: 'Ручка Master Pro LS внутренняя под цилиндр неокрашенная', qty: 1 },
+      { article: 'KN218NU_C', name: 'Ручка Master Pro LS внешняя под цилиндр неокрашенная', qty: 1 },
+      { article: 'KN100SO-115', name: 'Штифт 115 мм', qty: 1 },
+      { article: 'KN100TN-90', name: 'Пара винтов', qty: 1 },
+    ],
+  },
+};
+
+// Матрица фурнитуры для раздвижных систем
+const hardwareMatrixLiftSlide = [
+  {
+    minHeight: 2100, maxHeight: 2400,
+    minWidth: 720, maxWidth: 3300,
+    articles: [
+      { article: 'KN230AY-2300', name: 'Привод 2100-2400', qty: 1 },
+      { article: 'KN1678', name: 'Комплект кареток с угловой подкладкой', qty: 1 },
+      { article: 'KN1611', name: 'Комплект из крепежей, подкладок и упоров', qty: 1 },
+      { article: 'KN100QN-1400', name: 'Соединитель кареток 1500-2000', qty: 1 },
+      { article: '12001', name: 'Дистанционная подкладка зацепа 1мм', qty: 2 },
+      { article: '12002', name: 'Дистанционная подкладка зацепа 2мм', qty: 2 },
+    ],
+  },
+  {
+    minHeight: 2401, maxHeight: 2700,
+    minWidth: 720, maxWidth: 3300,
+    articles: [
+      { article: 'KN230AY-2600', name: 'Привод 2400-2700', qty: 1 },
+      { article: 'KN1678', name: 'Комплект кареток с угловой подкладкой', qty: 1 },
+      { article: 'KN1611', name: 'Комплект из крепежей, подкладок и упоров', qty: 1 },
+      { article: 'KN100QN-1400', name: 'Соединитель кареток 1500-2000', qty: 1 },
+      { article: '12001', name: 'Дистанционная подкладка зацепа 1мм', qty: 2 },
+      { article: '12002', name: 'Дистанционная подкладка зацепа 2мм', qty: 2 },
+    ],
+  },
+  {
+    minHeight: 2701, maxHeight: 3200,
+    minWidth: 720, maxWidth: 3300,
+    articles: [
+      { article: 'KN230AY-2600', name: 'Привод 2400-2700', qty: 1 },
+      { article: 'KN1678', name: 'Комплект кареток с угловой подкладкой', qty: 1 },
+      { article: 'KN1611', name: 'Комплект из крепежей, подкладок и упоров', qty: 1 },
+      { article: 'KN100QN-1400', name: 'Соединитель кареток 1500-2000', qty: 1 },
+      { article: 'KN100VO', name: 'Удлинитель привода 600', qty: 1 },
+      { article: 'KN100QV', name: 'Цапфа', qty: 1 },
+      { article: '12001', name: 'Дистанционная подкладка зацепа 1мм', qty: 4 },
+      { article: '12002', name: 'Дистанционная подкладка зацепа 2мм', qty: 4 },
+    ],
+  },
+];
