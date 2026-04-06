96b1dde5aed8de955808c0924c18b05a608f6623
FomaEd <164784372+FomaEd@users.noreply.github.com>
2026-04-06 13:53:12 +0300

    Update handles.js

--- data/handles.js
+++ data/handles.js
@@ -380,10 +380,3 @@ function getHandleArticles(handleType, handleColor, openingType, hardwareType) {
 
   return result;
 }
-
-function pickVisibleTurnTiltSet(height, width) {
-        return hardwareMatrixVisibleTurnTilt.find(row =>
-            height >= row.minHeight && height <= row.maxHeight &&
-            width >= row.minWidth && width <= row.maxWidth
-        );
-    }
